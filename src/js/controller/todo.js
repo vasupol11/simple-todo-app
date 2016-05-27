587bdfd4c0d73d81af5c03a964edc7681ea79286
Natchanon Charoensuk <inont@hotmail.co.th>
2016-05-27 09:11:27 +0700

    add ajax and show result

--- src/js/controller/todo.js
+++ src/js/controller/todo.js
@@ -1,9 +1,17 @@
 angular.module('todoApp', ['ui.router'])
-  .controller('TodoListController', function () {
-    var todoList = this
-    todoList.textarea = 'hello'
-    todoList.addTodo = function () {
-      //console.log(todoList.textarea)
-      console.log(todoList.textarea.split("\n"))
+  .controller('TodoListController', function ($http) {
+    self = this
+    self.userInput = ""
+    self.result = []
+    self.submit = function() {
+      self.result = []
+      var inputSentences = self.userInput.split('. ');
+      for(var i=0; i<inputSentences.length-1;i++) {
+        inputSentences[i] += '.'
+      }
+      $http.get('http://52.221.253.67:3000/get/pig').success(function (response) {
+          self.result = response
+          console.log(response);
+        })
     }
   })
