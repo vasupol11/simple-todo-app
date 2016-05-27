926c284917fdc423267cdb5765808a34da583188
Natchanon Charoensuk <inont@hotmail.co.th>
2016-05-27 09:38:51 +0700

    foxed post

--- src/js/controller/todo.js
+++ src/js/controller/todo.js
@@ -9,7 +9,9 @@ angular.module('todoApp', ['ui.router'])
       for(var i=0; i<inputSentences.length-1;i++) {
         inputSentences[i] += '.'
       }
-      $http.get('http://52.221.253.67:3000/get/pig').success(function (response) {
+      $http.post('http://52.221.253.67:3000/pig',{
+      	text: self.userInput
+      }).success(function (response) {
           self.result = response
           console.log(response);
         })
